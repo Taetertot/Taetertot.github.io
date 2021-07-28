3a3eb52c7083d4a981dfbfbbed2c9df8671aa12e
Taetertot <68134629+Taetertot@users.noreply.github.com>
2021-07-28 15:52:19 -0700

    Update button.js

--- button.js
+++ button.js
@@ -1,20 +1,25 @@
-$(document).ready(function(){
-     $(window).scroll(function () {
-            if ($(this).scrollTop() > 20) {
-                $('#back-to-top').fadeIn();
-            } else {
-                $('#back-to-top').fadeOut();
-            }
-        });
-        // scroll body to 0px on click
-        $('#back-to-top').click(function () {
-            $('#back-to-top').tooltip('hide');
-            $('body,html').animate({
-                scrollTop: 0
-            }, 300);
-            return false;
-        });
-        
-        $('#back-to-top').tooltip('show');
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
 
-});
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
