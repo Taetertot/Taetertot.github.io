d8b938d6f75133c57cbca24f9b7430613e796c32
Tae Jones <68134629+Taetertot@users.noreply.github.com>
2024-01-16 12:35:05 -0800

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -7,8 +7,15 @@ for (var i = 1; i <= 10; i++) {
   var thumb = document.createElement("img");
   thumb.src = "images/image" + i + ".jpg";
   thumb.alt = "Image " + i;
+  thumb.addEventListener(
+    "click", function() {
+      currentImage.src = this.src;
+      currentImage.alt = this.alt;
+    }
+  );
 }
 
+var currentImage = document.getElementById("current-image");
 
 let slideIndex = 1;
 showSlides(slideIndex);
