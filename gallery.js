b05f64dd1fcd53431ed53bf613e16d0c5e198cbf
Tae Jones <68134629+Taetertot@users.noreply.github.com>
2024-01-16 13:58:59 -0800

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -5,7 +5,7 @@ var imageThumbs = document.getElementById("image-thumbs");
 
 for (var i = 1; i <= 10; i++) {
   var thumb = document.createElement("img");
-  thumb.src = "images/image" + i + ".jpg";
+  thumb.src = "Images/image" + i + ".jpg";
   thumb.alt = "Image " + i;
   thumb.addEventListener(
     "click", function() {
