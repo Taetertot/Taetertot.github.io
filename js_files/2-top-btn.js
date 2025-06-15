aa7b0c58a1883d9f5046e47d578de77df402000b
Tae Jones <68134629+Taetertot@users.noreply.github.com>
2025-06-14 18:26:36 -0700

    Update 2-top-btn.js

--- js_files/2-top-btn.js
+++ js_files/2-top-btn.js
@@ -1,1 +1,12 @@
+<script>
+  const backToTop = document.getElementById("backToTop");
 
+ window.addEventListener('scroll', function () {
+  const button = document.getElementById('backToTop');
+  if (window.scrollY > 10) {
+    button.style.display = 'block';
+  } else {
+    button.style.display = 'none';
+  }
+});
+</script>
