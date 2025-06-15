0d8c68736c836c5aca8440cfa587b528493684a1
Tae Jones <68134629+Taetertot@users.noreply.github.com>
2025-06-14 18:40:01 -0700

    Create pics.js

---
+++ js_files/pics.js
@@ -0,0 +1,21 @@
+ let currentSlide = 0;
+        let currentTrack = 0;
+        const slides = document.querySelectorAll('.slides img');
+        const tracks = document.querySelectorAll('#audio-player source');
+        const audioPlayer = document.getElementById('audio-player');
+
+        function showSlide(index) {
+            currentSlide = index;
+            const slideContainer = document.getElementById('slides');
+            slideContainer.style.transform = `translateX(-${index * 100}%)`;
+        }
+
+        function nextSlide() {
+            currentSlide = (currentSlide + 1) % slides.length;
+            showSlide(currentSlide);
+        }
+
+        function prevSlide() {
+            currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+            showSlide(currentSlide);
+        }
