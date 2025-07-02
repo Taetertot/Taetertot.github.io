b43a3ba9cb94c7a075ac6a40c036dfb3404610de
Tae Jones <68134629+Taetertot@users.noreply.github.com>
2025-07-01 23:28:52 -0700

    Update schedule-btn.js

--- js_files/schedule-btn.js
+++ js_files/schedule-btn.js
@@ -1,10 +1,14 @@
- document.addEventListener('DOMContentLoaded', function () {
-      var target = document.currentScript.parentElement;
-      window.addEventListener('load', function () {
-        calendar.schedulingButton.load({
-          url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ0nxw-f8qrIcLH3oVHLmMwtr7Dsz7um2pP8TL8I36CczVeiz94qjv4DlhAzLdKer6T4VI3dxdRV?gv=true',
-          label: "Schedule to meet",
-          target,
-        });
-      });
-    })();
+window.addEventListener('load', function () {
+  const target = document.getElementById('schedule-button-container');
+  
+  if (window.calendar && calendar.schedulingButton && typeof calendar.schedulingButton.load === 'function') {
+    calendar.schedulingButton.load({
+      url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ0nxw-f8qrIcLH3oVHLmMwtr7Dsz7um2pP8TL8I36CczVeiz94qjv4DlhAzLdKer6T4VI3dxdRV?gv=true',
+      color: '#0B8043',
+      label: 'Schedule to meet',
+      target: target,
+    });
+  } else {
+    console.error('Google Calendar scheduling button script is not ready.');
+  }
+});
