a11bb3a956021378387616a8948b3def9bdc6bd8
Tae Jones <68134629+Taetertot@users.noreply.github.com>
2025-07-01 18:25:13 -0700

    Update 2-top-btn.js
    Fixing again

--- js_files/2-top-btn.js
+++ js_files/2-top-btn.js
@@ -1,11 +1,12 @@
 <script>
- const backToTop = document.getElementById("backToTop");
-
-window.addEventListener('scroll', function () {
-  if (window.scrollY > 10) {
-    backToTop.style.display = 'block';
-  } else {
-    backToTop.style.display = 'none';
-  }
+document.addEventListener('DOMContentLoaded', function() {
+  const backToTop = document.getElementById("backToTop");
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 10) {
+      backToTop.style.display = 'block';
+    } else {
+      backToTop.style.display = 'none';
+    }
+  });
 });
 </script>
