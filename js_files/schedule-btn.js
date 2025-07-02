138f9e2e96ac4577890c508dd0ab4fa49e5015de
Tae Jones <68134629+Taetertot@users.noreply.github.com>
2025-07-01 23:19:37 -0700

    Update schedule-btn.js

--- js_files/schedule-btn.js
+++ js_files/schedule-btn.js
@@ -3,7 +3,6 @@
       window.addEventListener('load', function () {
         calendar.schedulingButton.load({
           url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ0nxw-f8qrIcLH3oVHLmMwtr7Dsz7um2pP8TL8I36CczVeiz94qjv4DlhAzLdKer6T4VI3dxdRV?gv=true',
-          color: '#0B8043',
           label: "Schedule to meet",
           target,
         });
